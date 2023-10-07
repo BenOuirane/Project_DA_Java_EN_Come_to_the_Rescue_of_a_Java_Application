53ed93b9a4b1c7897d83d54184a8ab42fc4b63c8
HBO <benouiranehajour@gmail.com>
2023-10-07 13:39:43 +0200

    writer implementation

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,13 +1,24 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.nio.file.Files;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 import javax.swing.text.html.parser.Parser;
 
@@ -16,9 +27,15 @@ public class AnalyticsCounter {
 	// private static int rashCount = 0;		// initialize to 0
 	// private static int pupilCount = 0;		// initialize to 0
 	
-
+	 final static String outputFilePath 
+     = "..\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\result.out"; 
 	
 	public static void main(String args[]) throws Exception {
+    	Hashtable<String, Integer> symptome = new Hashtable<String, Integer>();
+
+		WriteSymptomDataToFile test = new WriteSymptomDataToFile();
+		
+		test.writeSymptoms(symptome);
 		
 		   // File path is passed as parameter
         File file = new File(
@@ -36,11 +53,104 @@ public class AnalyticsCounter {
         String st;
         // Condition holds true till
         // there is character in a string
-        while ((st = br.readLine()) != null)
+    //    while ((st = br.readLine()) != null)
  
             // Print the string
-            System.out.println(st);
-    }
+     //       System.out.println(st);
+        
+        
+      //  String[] words = br.split("\\s+|\\.|\\,|\\?|\\:|\\;");
+    	Hashtable<String, Integer> wordCountTable = new Hashtable<String, Integer>();
+
+    	int minWordLength = 2;
+    	 while ((st = br.readLine()) != null)
+    	{
+    		String uniqueWord = st.toLowerCase();
+    		if (uniqueWord.length() > minWordLength) 
+    		{
+    			if (wordCountTable.containsKey(uniqueWord))	{
+    				wordCountTable.replace(uniqueWord, wordCountTable.get(uniqueWord),
+    						wordCountTable.get(uniqueWord).intValue() + 1);
+    			} else {
+    				wordCountTable.put(uniqueWord, 1);
+    			}
+
+    		}
+    	}
+    
+    	wordCountTable.entrySet().forEach(entry -> {
+    	//	System.out.println(entry.getKey() + ": " + entry.getValue());
+    	});
+    	
+    	          
+    
+    	
+    	   String text = "Computer Science Portal GeeksforGeeks";
+         
+    	   
+    	 try {
+    		 
+             // Step 1: Create an object of BufferedWriter
+             BufferedWriter f_writer
+                 = new BufferedWriter(new FileWriter(
+                		 outputFilePath));
+             
+             for (Entry<String, Integer> entry : 
+            	 wordCountTable.entrySet()) { 
+  
+                // put key and value separated by a colon 
+            	 f_writer.write(entry.getKey() + ":"
+                         + entry.getValue()); 
+  
+                // new line 
+            	 f_writer.newLine(); 
+            }  
+
+             // Step 2: Write text(content) to file
+        //     f_writer.write(text);
+
+
+             // Step 3: Printing the content inside the file
+             // on the terminal/CMD
+           //  System.out.print(wordCountTable);
+
+             // Step 4: Display message showcasing
+             // successful execution of the program
+        //     System.out.print( "File is created successfully with the content.");
+
+             // Step 5: Close the BufferedWriter object
+             f_writer.close();
+         }
+
+         // Catch block to handle if exceptions occurs
+         catch (IOException e) {
+
+             // Print the exception on console
+             // using getMessage() method
+             System.out.print(e.getMessage());
+         }
+     }
+        
+    
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	//	 BufferedReader reader = new BufferedReader (new FileReader("..\\Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application\\Project02Eclipse\\src\\com\\hemebiotech\\analytics\\symptoms.txt"));
 	//		String line = reader.readLine();
@@ -92,5 +202,5 @@ public class AnalyticsCounter {
 
 //	}	
 		 
-	
 }
+
